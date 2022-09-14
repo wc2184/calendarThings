2215f1e0e80bb70377496fea61e90ae07d5bac49
William Chan <wc2184@nyu.edu>
2022-09-13 23:05:55 -0400

    .

--- src/calendardata/Viewagenda.js
+++ src/calendardata/Viewagenda.js
@@ -101,7 +101,7 @@ const Viewagenda = ({ tasks, setUpdate, user }) => {
                     {/* <Td>{task.id}</Td> */}
                     <Td maxW="50vw" sx={{ display: 'flex' }}>
                       {window.navigator.userAgent.match(/iPhone/i) ? (
-                        <Select maxW="4vw" placeholder="i">
+                        <Select maxW="8vw" placeholder={task.first}>
                           <option>{task.first}</option>
                         </Select>
                       ) : null}
